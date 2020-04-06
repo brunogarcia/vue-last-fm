f327d618aca8770d53cdd02ce901b9155debe555
Bruno Garcia <bruno@garciaechegaray.com>
2020-04-06 20:02:54 +0200

    Add loader for the album list

--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -1,17 +1,20 @@
 <template>
   <v-container>
-    <AlbumsComponent v-if="!loading" />
+    <AlbumsLoader v-if="loading" />
+    <AlbumsComponent v-else />
   </v-container>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
 import AlbumsComponent from '@/components/Albums.vue';
+import AlbumsLoader from '@/components/AlbumsLoader.vue';
 
 export default {
   name: 'AlbumsView',
 
   components: {
+    AlbumsLoader,
     AlbumsComponent,
   },
 
