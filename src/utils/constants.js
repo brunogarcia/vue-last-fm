e1c2450d898d68ad90415a6bf1c294db9ec83868
Bruno Garcia <bruno@garciaechegaray.com>
2020-04-07 11:41:15 +0200

    Add https to the API host

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export default {
   API: {
-    HOST: 'http://ws.audioscrobbler.com/2.0/',
+    HOST: 'https://ws.audioscrobbler.com/2.0/',
     KEY: 'cd5651d9a6c60934a62baafc6290e70c',
   },
   IMAGES: {
